cc26c260d5cadaa1e70ad2e3d14c6854185825e8
Apvictor <armandinho14.ap@gmail.com>
2021-04-14 23:05:41 -0300

    Primeiro envio 14/04/2021

---
+++ readme.md
@@ -0,0 +1,52 @@
+<!-- CAPA OU LOGO -->
+<h1>
+<img src="https://ik.imagekit.io/mnbr5uwksus/ADSoftware/banner-ads_HKJ-B9hUel.jpg">
+</h1>
+
+<!-- DESCRIÇÃO -->
+<center><h1>
+<img rel="icon" src="https://ik.imagekit.io/mnbr5uwksus/Icons/icon_1GAtanMp0.png" type="image/gif"><br>
+Projeto - Lista de Tarefas
+</h1></center>
+
+<p>Olá, seja bem vindo! ✌</p>
+
+<p>Sou Armando Pereira, desenvolvedor de software na Simonini Software, atualmente estou no último semestre da Faculdade de Análise e Desenvolvimento de Sistemas.<br>
+Disponível para atuar no mercado de desenvolvimento de sistemas.</p>
+
+<!-- GIF -->
+<img rel="icon" src="../src/assets/telas/animationTelas.gif" type="image/gif"><br>
+
+<ul>
+  <li>Principais Tecnologias Utilizadas: Ionic 5, TypeScript e CSS.<br></li>
+</ul>
+
+<!-- FINALIDADE DO PROJETO -->
+<h3>Finalidade do Projeto </h3>
+<p>
+Quem nunca se pegou cheio de taferas e não sabia como organizá-las?<br>
+Foi pensando neste propósito que decidi desenvolver uma aplicação para organizar as minhas tarefas diárias.<br>
+O projeto consiste em um Aplicativo para organizar tarefas.
+</p>
+
+<!-- COMO UTILIZAR -->
+<h3>Como Utilizar </h3>
+
+<ol>
+<li>Ao iniciar o aplicativo será exibido uma mensagem "Não há tarefas".</li>
+<li>Clicar no botão + para cadastrar tarefas</li>
+<li>Clicar na tarefa para finalizar ou desmarcar a finalização</li>
+<li>Arrastar a tarefa para a direita para deletar</li>
+</ol>
+
+<!-- ESTUDO ATUAL -->
+<p>Atualmente estudando: Ionic 5 e TypeScript.</p>
+
+<!-- LinkedIn -->
+<a href="www.linkedin.com/in/armando-víctor-pereira-2021"><img rel="icon" src="https://ik.imagekit.io/mnbr5uwksus/Icons/linkedin-16_HxYEL0Hyz.png" type="image/gif"> <b>LinkedIn</b></a>
+
+<!-- GitHub -->
+<a aling="center" href="https://github.com/Apvictor"><img rel="icon" src="https://ik.imagekit.io/mnbr5uwksus/Icons/github-azul-16_9qp9n_vdj.png" type="image/gif"> <b>GitHub</b></a>
+
+<hr>
+<center>&copy 2021 Armando Pereira </center>
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,84 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  tasks: any[] = [];
+
+  constructor(private actionSheetCtrl: ActionSheetController, private toastCtrl: ToastController, private alertCtrl: AlertController) {
+    let tasksJson = localStorage.getItem('tasksDB');
+    if (tasksJson != null) {
+      this.tasks = JSON.parse(tasksJson);
+    }
+  }
+
+  async openActions(task: any) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: "Tarefa concluída?",
+      buttons: [{
+        text: task.done ? 'Desmarcar' : 'Marcar',
+        icon: task.done ? 'radio-button-off' : 'checkmark',
+        handler: () => {
+          task.done = !task.done;
+          this.updateLocalStorage();
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        role: 'cancel'
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('tasksDB', JSON.stringify(this.tasks));
+  }
+
+  delete(task: any) {
+    this.tasks = this.tasks.filter(taskArray => task != taskArray);
+
+    this.updateLocalStorage();
+  }
+
+  async showAdd() {
+    const alert = await this.alertCtrl.create({
+      header: 'O que deseja fazer?',
+      inputs: [{
+        name: 'taskToDo',
+        type: 'text',
+        placeholder: 'Adicione uma tarefa'
+      }],
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+        cssClass: 'secondary',
+      },
+      {
+        text: 'Adicionar',
+        handler: (form) => {
+          this.add(form.taskToDo);
+        }
+      }]
+    });
+    await alert.present();
+  }
+
+  async add(taskToDo: string) {
+    if (taskToDo.trim().length < 1) {
+      const toast = await this.toastCtrl.create({
+        message: 'Informe o que deseja fazer!',
+        duration: 2000,
+        position: 'bottom',
+      });
+      toast.present();
+      return;
+    }
+
+    let task = { name: taskToDo, done: false };
+
+    this.tasks.push(task);
+
+    this.updateLocalStorage();
+
+  }
 
 }
